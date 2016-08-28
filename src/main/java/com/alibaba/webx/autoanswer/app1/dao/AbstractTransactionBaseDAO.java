01bf998bff658046e59ebe74332ad4a13073deac
¾´µÀ <xuzhen.qxz@alibaba-inc.com>
2016-08-28 16:28:25 +0800

    myspace

--- src/main/java/com/alibaba/webx/autoanswer/app1/dao/AbstractTransactionBaseDAO.java
+++ src/main/java/com/alibaba/webx/autoanswer/app1/dao/AbstractTransactionBaseDAO.java
@@ -2,9 +2,8 @@ package com.alibaba.webx.autoanswer.app1.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 import org.springframework.stereotype.Component;
-
+import com.ibatis.sqlmap.client.SqlMapClient;
 
 @Component("abstractTransactionBaseDAO")
 public class AbstractTransactionBaseDAO extends SqlMapClientDaoSupport {
@@ -12,5 +11,5 @@ public class AbstractTransactionBaseDAO extends SqlMapClientDaoSupport {
     public void setBaseSqlMapClient(@Qualifier("workplatfromSqlMapClient") SqlMapClient  sqlMapClient){
         this.setSqlMapClient(sqlMapClient);
     }
-
 }
+
